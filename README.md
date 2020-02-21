43571f2d4882316915381bcf63b3d9c49c3e717f
Petr Messner <petr.messner@gmail.com>
2020-02-21 11:43:48 +0100

    add prototype

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Links
 
 - https://requests.readthedocs.io/en/master/
 - https://requests.readthedocs.io/en/master/user/advanced/#example-specific-ssl-version
+- https://lukasa.co.uk/2013/01/Choosing_SSL_Version_In_Requests/
+- https://github.com/psf/requests/blob/master/requests/adapters.py
+- https://github.com/psf/requests/issues/3774#issuecomment-267871876
+- https://github.com/urllib3/urllib3/blob/master/src/urllib3/poolmanager.py
---
+++ tests/test_prototype.py
@@ -0,0 +1,60 @@
+from pytest import mark
+
+
+@mark.parametrize('hostname', ['google.com', 'cbaccesscontrol.xdl.dk'])
+def test_adapter_with_ssl_version(hostname):
+    import ssl
+    from urllib3.poolmanager import PoolManager
+    from requests import Session
+    from requests.adapters import HTTPAdapter
+
+    assert HTTPAdapter.init_poolmanager
+
+    class CustomHttpAdapter (HTTPAdapter):
+        '''Transport adapter" that allows us to use custom ssl_version.'''
+
+        def __init__(self, ssl_version=None, **kwargs):
+            self.ssl_version = ssl_version
+            super().__init__(**kwargs)
+
+        def init_poolmanager(self, connections, maxsize, block=False):
+            self.poolmanager = PoolManager(
+                num_pools=connections, maxsize=maxsize,
+                block=block, ssl_version=self.ssl_version)
+
+    s = Session()
+    s.mount('https://', CustomHttpAdapter(ssl.PROTOCOL_SSLv23))
+    url = 'https://' + hostname
+    r = s.get(url)
+    r.raise_for_status()
+
+
+@mark.parametrize('hostname', ['google.com', 'cbaccesscontrol.xdl.dk'])
+def test_adapter_with_ssl_context(hostname):
+    import ssl
+    from urllib3.poolmanager import PoolManager
+    from requests import Session
+    from requests.adapters import HTTPAdapter
+
+    assert HTTPAdapter.init_poolmanager
+
+    class CustomHttpAdapter (HTTPAdapter):
+        '''Transport adapter" that allows us to use custom ssl_context.'''
+
+        def __init__(self, ssl_context=None, **kwargs):
+            self.ssl_context = ssl_context
+            super().__init__(**kwargs)
+
+        def init_poolmanager(self, connections, maxsize, block=False):
+            self.poolmanager = PoolManager(
+                num_pools=connections, maxsize=maxsize,
+                block=block, ssl_context=self.ssl_context)
+
+    s = Session()
+    ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    ctx.options &= ~ssl.OP_NO_SSLv3
+    ctx.options &= ~ssl.OP_NO_SSLv2
+    s.mount('https://', CustomHttpAdapter(ctx))
+    url = 'https://' + hostname
+    r = s.get(url)
+    r.raise_for_status()
